1a9222f8213a36223a3479e5b108fc8f73552cb5
martin machiebe <martinmachiebe21@gmail.com>
2025-02-04 16:35:40 +0100

    updates

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,12 +1,12 @@
-// import Image from "next/image";
+import Image from "next/image";
 import React from "react";
-// import aiLogo from "../../../public/AI ONE Purple 1@2x.png";
+import aiLogo from "../../../public/AI ONE ash 001@3x.png";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center text-white backdrop-blur-[10px] bg-[#bbdaf31c] my-6 rounded-lg px-6 py-6 mx-8">
-      <h1 className="text-[#5CE1E6] italic text-xl">AiOne</h1>
-      {/* <Image src={aiLogo} alt="AiOne Logo" width={80} height={80} /> */}
+      {/* <h1 className="text-[#5CE1E6] italic text-xl">AiOne</h1> */}
+      <Image src={aiLogo} alt="AiOne Logo" width={80} height={80} />
       <div className="flex justify-center text-lg items-center space-x-6">
         <p className=" cursor-pointer hover:text-[#5ce1e6] transition-all">
           Community
@@ -15,7 +15,9 @@ const Navbar = () => {
           Data
         </p>
       </div>
-      <button className=" text-lg">Connect Wallet</button>
+      <button className=" border px-4 py-2 hover:bg-[#5ce1e6] hover:border-none rounded-lg border-1 text-lg">
+        Connect Wallet
+      </button>
     </div>
   );
 };
