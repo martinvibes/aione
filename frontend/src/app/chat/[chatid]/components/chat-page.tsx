ac225d354b45c472bd5644c9f3f1b69c5fad63ba
“Akshola00” <akinniyishola07@gmail.com>
2025-02-19 23:03:58 +0100

    feat: fix build

--- frontend/src/app/chat/[chatid]/components/chat-page.tsx
+++ frontend/src/app/chat/[chatid]/components/chat-page.tsx
@@ -2,25 +2,21 @@
 import React, { useContext } from "react";
 import { MessageContext } from "@/app/useContext/message-context";
 import RugCheckComponent from "./rug-check";
-
 import TradingInfo from "./trading-info";
-
 import SkeletonCard from "@/app/components/ui/skeleton";
-
 
 export const Chatpage = () => {
   const { messages, isLoading } = useContext(MessageContext);
 
   return (
     <>
-      <div
-        className={`text h-[90%] w-full max-w-7xl chat-texts mx-auto p-4 rounded-lg space-y-4 flex flex-col-reverse overflow-y-auto scrollbar-hide scroll-smooth  relative  z-[555]`}
-      >
+      <div className="text h-[90%] w-full max-w-7xl chat-texts mx-auto p-4 rounded-lg space-y-4 flex flex-col-reverse overflow-y-auto scrollbar-hide scroll-smooth relative z-[555]">
         {[...messages].reverse().map((message) => (
+          <div
             key={message.id}
             className={`flex items-center my-2 ${
               message.sender === "user" ? "justify-end" : ""
-            }  relative  z-[555]`}
+            } relative z-[555]`}
           >
             <div
               className={`${
@@ -29,13 +25,13 @@ export const Chatpage = () => {
                   : "rounded-[30px] rounded-es-none"
               } p-5 break-words bg-[#1C2535] overflow-wrap-anywhere max-w-[50%]`}
             >
-              {message.sender == "user" && (
+              {message.sender === "user" && (
                 <div
                   dangerouslySetInnerHTML={{ __html: `${message.content}` }}
                   className="message-content relative h-fit"
                 />
               )}
-              {message.sender == "agent" && (
+              {message.sender === "agent" && (
                 <div key={message.id} className="message-content relative pl-3">
                   <div
                     className={`${
@@ -45,17 +41,17 @@ export const Chatpage = () => {
                     } w-3 h-3 absolute -left-1 rounded-full`}
                   />
                   {message.content}
-                
-                                  {message.component && (
-                  <div className="mt-4">
-                    {message.component.type === "RugCheck" && (
-                      <RugCheckComponent {...message.component.props} />
-                    )}
-                    {message.component.type === "chart" && (
-                      <TradingInfo {...message.component.props} />
-                    )}
-                  </div>
-                )}
+                  
+                  {message.component && (
+                    <div className="mt-4">
+                      {message.component.type === "RugCheck" && (
+                        <RugCheckComponent {...message.component.props} />
+                      )}
+                      {message.component.type === "chart" && (
+                        <TradingInfo {...message.component.props} />
+                      )}
+                    </div>
+                  )}
                 </div>
               )}
             </div>
